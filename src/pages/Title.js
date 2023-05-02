711e85ee2b5ec43eae3c4f6420d5781d37a557b0
gabbya8 <gabriellaalexis2399@gmail.com>
2023-05-02 18:19:54 -0400

    removed button component and implemented routing

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -1,6 +1,10 @@
 import './css/Title.css';
 import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
+
+
 function Title() {
+    const nav = useNavigate()
     return(
         <div className="Title">
             <header className='Title-header'>
@@ -8,7 +12,11 @@ function Title() {
                     <p>sample site</p>
                 </div>
                 <p className='intro'>Welcome to the sample site! This app let's you search for your favorite songs and see what sample they have used to create it. You'll be able to listen to the tracks and see where the sampling happens on the next page. Click the start button to begin!</p>
-                <Button text='Start'></Button>
+                <button className='button-27' onClick={() => nav('main')}>
+                    <div className='text'>
+                        Start
+                    </div>
+                </button>
             </header>
         </div>
     );
