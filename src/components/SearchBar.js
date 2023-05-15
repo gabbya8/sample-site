6e5e5c3226d277d7b778e6274a2f23d939260028
gabbya8 <gabriellaalexis2399@gmail.com>
2023-05-15 16:01:43 -0400

    search bar event handling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,18 @@ import './css/SearchBar.css'
 import {FaSistrix} from "react-icons/fa";
 
 
+
 function SearchBar() {
+    const handleClick = (e) => {
+        e.preventDefault();
+        let text = document.getElementById("search-input").value;
+        console.log(text.toLowerCase().split(' ').join('-'));
+      }
     return(
         <div className="search-bar">
             <form>
-                <input placeholder="Search..."></input>
-                <button type="submit"><FaSistrix /></button>
+                <input id="search-input" placeholder="Search..." autoComplete='off'></input>
+                <button type="submit" onClick={handleClick}><FaSistrix /></button>
             </form>
         </div>
     );
